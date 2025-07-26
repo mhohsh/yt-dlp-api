42bec16b3aeb29acd3255921890784851024d36a
mhohsh <152607375+mhohsh@users.noreply.github.com>
2025-07-26 04:04:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import yt_dlp
 
 app = Flask(__name__)
 
-@app.route('/extract', methods=['GET'])
-def extract():
-    video_url = request.args.get('url')
-    if not video_url:
-        return jsonify({'error': 'URL is required'}), 400
-
+@app.route('/', methods=['POST'])
+def get_direct_url():
     try:
+        data = request.get_json()
+        video_url = data.get('url')
+        if not video_url:
+            return jsonify({'error': 'Missing URL'}), 400
+
         ydl_opts = {
             'quiet': True,
             'skip_download': True,
@@ -20,8 +21,9 @@ def extract():
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=False)
             return jsonify({'direct_url': info['url']})
-
+    
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-app.run(host='0.0.0.0', port=3000)
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=3000)
